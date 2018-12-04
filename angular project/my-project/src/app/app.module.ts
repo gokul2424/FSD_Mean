e96d5722743fc29bc72b24deabace9f3b29907a9
unknown <gokulpraveen12@gmail.com>
2018-12-04 16:45:25 +0530

    cardview angular

--- angular project/my-project/src/app/app.module.ts
+++ angular project/my-project/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BadgeComponent } from './badge.component';
+import { CardComponent } from './card.component';
 
 @NgModule({
   declarations: [
-    AppComponent, BadgeComponent 
+    AppComponent, BadgeComponent , CardComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule
 
   ],
   providers: [],
